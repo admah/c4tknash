8bb633bf775cc54a60a18c83fc6d1d82bf21448a
Adam Murray - US <admurray@worldvision.org>
2016-02-02 17:36:52 -0600

    Initial commit of current c4tknash files.

---
+++ js/js_DaAQNkgEE4nHSkhtM9FqIwt2oJrNTkN9QnKNALDIz9o.js
@@ -0,0 +1,21 @@
+(function ($) {
+Drupal.behaviors.agencyTheme = {
+  attach: function (context) {
+    // Replace cbpAnimatedHeader.js
+    $(window).scroll(function(){ console.log(location.pathname);
+        if ($(this).scrollTop() > 200 || location.pathname == '/code-of-conduct.html' || location.pathname == '/challenges.html' || location.pathname == '/awards.html' || location.pathname == '/format.html'){
+            $('.navbar-default').addClass('navbar-shrink');
+        }
+        else{
+            $('.navbar-default').removeClass('navbar-shrink');
+        }
+    });
+
+    // Hide the responive menu when clicking in a menu item
+    $('#navbar .navbar-collapse ul li a').click(function() {
+        $('#navbar button.navbar-toggle:visible').click();
+    });
+  }
+};
+})(jQuery);
+;
