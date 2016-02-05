db721930de0efbc768ea0297f537fd7ff4078535
Adam Murray - US <admurray@worldvision.org>
2016-02-04 20:39:03 -0600

    Add API page.  Update menus. Add API page to site map.

--- js/js_DaAQNkgEE4nHSkhtM9FqIwt2oJrNTkN9QnKNALDIz9o.js
+++ js/js_DaAQNkgEE4nHSkhtM9FqIwt2oJrNTkN9QnKNALDIz9o.js
@@ -3,7 +3,7 @@ Drupal.behaviors.agencyTheme = {
   attach: function (context) {
     // Replace cbpAnimatedHeader.js
     $(window).scroll(function(){ console.log(location.pathname);
-        if ($(this).scrollTop() > 200 || location.pathname == '/code-of-conduct.html' || location.pathname == '/challenges.html' || location.pathname == '/awards.html' || location.pathname == '/format.html'){
+        if ($(this).scrollTop() > 200 || location.pathname == '/code-of-conduct.html' || location.pathname == '/challenges.html' || location.pathname == '/awards.html' || location.pathname == '/api.html'){
             $('.navbar-default').addClass('navbar-shrink');
         }
         else{
