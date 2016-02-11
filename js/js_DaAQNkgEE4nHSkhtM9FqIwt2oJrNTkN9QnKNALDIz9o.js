d3f42dfd75127ccee88d4b38bcb6c532a2296ed6
Adam Murray - US <admurray@worldvision.org>
2016-02-11 10:47:54 -0600

    Update index.html to include speaker info.  Clean up code on index.html.  Remove console.log in js file.

--- js/js_DaAQNkgEE4nHSkhtM9FqIwt2oJrNTkN9QnKNALDIz9o.js
+++ js/js_DaAQNkgEE4nHSkhtM9FqIwt2oJrNTkN9QnKNALDIz9o.js
@@ -2,7 +2,7 @@
 Drupal.behaviors.agencyTheme = {
   attach: function (context) {
     // Replace cbpAnimatedHeader.js
-    $(window).scroll(function(){ console.log(location.pathname);
+    $(window).scroll(function(){
         if ($(this).scrollTop() > 200 || location.pathname == '/code-of-conduct.html' || location.pathname == '/challenges.html' || location.pathname == '/awards.html' || location.pathname == '/api.html'){
             $('.navbar-default').addClass('navbar-shrink');
         }
